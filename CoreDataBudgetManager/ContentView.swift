3a7c92dbffd87160a6452b91f6c671250c95075f
Moin UD-din <129534945+MoinUddin-dev-dot@users.noreply.github.com>
2025-05-26 01:39:11 +0500

    Initial Commit

---
+++ CoreDataBudgetManager/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CoreDataBudgetManager
+//
+//  Created by Moin on 5/26/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CoreDataBudgetManager/CoreDataBudgetManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataBudgetManagerApp.swift
+//  CoreDataBudgetManager
+//
+//  Created by Moin on 5/26/25.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataBudgetManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
